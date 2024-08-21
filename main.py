e5eab10ea309d051569f0e465d56db8108c90ac3
ChristopherKildea <122947271+ChristopherKildea@users.noreply.github.com>
2024-08-20 23:13:14 -0400

    FAdded photo taking abilities

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-
-# first, let's work on detecting when someone first enters the frame and take a picture of them
-# (Still using the mall so far until I get this figured out)
-
-
-# https://blog.roboflow.com/how-to-count-objects-in-a-zone/
-
 import numpy as np
 import supervision as sv
 import cv2
@@ -14,8 +7,22 @@ from ultralytics import YOLO
 model = YOLO("yolov8s.pt")
 tracker = sv.ByteTrack()
 
-MALL_VIDEO_PATH = 'videos/mall.mp4'
+# Path to the video you want to annotate
+VIDEO_PATH = 'videos/thief_video3.mp4'
 
+
+# Different polygon zones corresponding
+# to different example videos
+polygon = np.array([
+    [51, 354],
+    [203, 98],
+    [407, 102],
+    [495, 354],
+    [51, 354]
+])
+
+# Polygon area for mall video
+""""
 polygon = np.array([
    [652, 1071],
    [960, 619],
@@ -23,9 +30,35 @@ polygon = np.array([
    [1752, 1075],
    [652, 1071]
 ])
+"""
 
+"""
+# Polygon area for thief_video2.mp4
+polygon = np.array([
+    [1137, 374],
+    [725, 238],
+    [697, 258],
+    [521, 190],
+    [1, 418],
+    [1, 514],
+    [113, 714],
+    [1013, 718],
+    [1141, 370]
+])
+"""
 
+"""
+# Polygon area for thief_video.mp4
+polygon = np.array([
+    [247, 351],
+    [304, 105],
+    [393, 105],
+    [516, 355],
+    [247, 355]
+])
+"""
 
+# Polygon zone of which we are detecting objects in
 zone = sv.PolygonZone(polygon=polygon)
 
 # initiate annotators
@@ -34,23 +67,25 @@ label_annotator = sv.LabelAnnotator(text_thickness=4, text_scale=2)
 zone_annotator = sv.PolygonZoneAnnotator(zone=zone, color=sv.Color.WHITE, thickness=6, text_thickness=6, text_scale=4)
 
 
+# Number of photos taken
 photo_count = 0
+# Each person entering the frame is assigned an i.d.
 captured_ids = set()
-# Tells supervision how to process each frame in our video
+
+
+# Describes how to process each frame of the video
 def process_frame(frame: np.ndarray, _) -> np.ndarray:
     global photo_count
 
     # detect
     results = model(frame, imgsz=1280)[0]
     detections = sv.Detections.from_ultralytics(results)
-    detections = detections[detections.class_id == 0]
+    detections = detections[detections.class_id == 0]  # 0 class i.d. is 'person'
     detections = tracker.update_with_detections(detections)
     # Sees if detections are in zone
     zone.trigger(detections=detections)
 
-    # annotate
-    #labels = [f"{model.names[class_id]} {confidence:0.2f}" for _, _, confidence, class_id, _, _ in detections]
-
+    # What we are labeling
     labels = [
         f"#{tracker_id} {results.names[class_id]}"
         for class_id, tracker_id
@@ -61,13 +96,11 @@ def process_frame(frame: np.ndarray, _) -> np.ndarray:
     frame = label_annotator.annotate(scene=frame, detections=detections, labels=labels)
     frame = zone_annotator.annotate(scene=frame)
 
-
     for i in range(len(detections.xyxy)):
         tracker_id = detections.tracker_id[i] if detections.tracker_id is not None else None
         if tracker_id is not None and tracker_id not in captured_ids:
             captured_ids.add(tracker_id)
             photo_count += 1
-
 
             x1 = int(detections.xyxy[i][0])
             y1 = int(detections.xyxy[i][1])
@@ -79,16 +112,12 @@ def process_frame(frame: np.ndarray, _) -> np.ndarray:
             # Save the frame with the new person detected
             photo_path = f"detected_persons/detected_person_{photo_count}.jpg"
 
-            #cv2.imwrite(photo_path, frame)
             cv2.imwrite(photo_path, cropped_image)
-
-
-
 
     return frame
 
 
-sv.process_video(source_path=MALL_VIDEO_PATH, target_path=f"mall-result.mp4", callback=process_frame)
+sv.process_video(source_path=VIDEO_PATH, target_path=f"result.mp4", callback=process_frame)
 
 from IPython import display
 display.clear_output()
